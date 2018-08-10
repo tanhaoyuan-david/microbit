74369ef0d03e7bcbe76b6a5e7c23dc368a5c8242
tanhaoyuan-david <39994498+tanhaoyuan-david@users.noreply.github.com>
2018-08-10 21:01:35 +0800

    Create image_display.py

---
+++ image_display.py
@@ -0,0 +1,11 @@
+#disaplay a digit "2"
+
+from microbit import *
+#display digit one
+boat = Image("99999:"
+             "00009:"
+             "99999:"
+             "90000:"
+             "99999:")
+             
+display.show(boat)
